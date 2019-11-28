819f264083b1969cc7f0618ac3ba19697c66e637
lina2497 <alex.betts@evobio.eu>
2019-11-28 10:08:02 +0000

    Add curl call for IP addresses

--- R/lazy_proxy_connect.r
+++ R/lazy_proxy_connect.r
@@ -1,4 +1,5 @@
 
+
 #' Connect through proxy with Demeter credentials in environment
 #'
 #' @param username Your demeter username.
@@ -7,29 +8,33 @@
 #' @return Allows internet connectivity, e.g. APIs, webscraping, installing packages from github.
 #' @examples
 #' lazy_proxy_connect(username="m123456", password="password")
-#' 
+#'
 #' @export
 
 
 
 
-lazy_proxy_connect<-function(username="m123456",password="password"){
-  
+lazy_proxy_connect <- function(username = "m123456",
+                               password = "password",
+                               url = "http://google.com") {
   #I've called this function lazy because it will allow the user
   #to save their username and password in an r script
-  #and not have to type it in each time. 
+  #and not have to type it in each time.
   #Writing your password down is generally a bad idea but this function
-  #may be useful for automation. I reccomend saving your credentials in 
+  #may be useful for automation. I reccomend saving your credentials in
   #an .rdata or .rds of a list(username = x, password = x) then reading it in.
   #This way your password won't be visible on your screen whilst you work.
   
   
+  #If a URL is supplied, the proxy address is retrieved for it, otherwise it defaults to an external IP
+  proxy_ip <-
+    stringr::str_split(curl::ie_get_proxy_for_url(url), ";")[[1]][1]
+  
   #This combines your username and password with the IP address of the proxy
-  proxy_address<-paste0("http://",username,":",password,"@148.253.235.216:80")
+  proxy_address <-
+    paste0("http://", username, ":", password, "@", proxy_ip)
   
   #This creates envirnmnet variables with the proxy details allowing R to connect.
-  Sys.setenv(http_proxy=proxy_address,
-             https_proxy=proxy_address)
+  Sys.setenv(http_proxy = proxy_address,
+             https_proxy = proxy_address)
 }
-
-
--- R/proxy_connect.r
+++ R/proxy_connect.r
@@ -18,32 +18,36 @@
 
 #This Script will allow you to connect to API's and webscrape on demeter.
 
-
-proxy_connect<-function(show_characters=FALSE){
-  
-  #Asks for username
-  ifelse(
-    show_characters,
-    username <-
-      readline(prompt = "Enter Username:"),
-    username <- getPass::getPass(msg = "Username", noblank = T)
-  )
-  
-  #Asks for password
-  ifelse(
-    show_characters,
-    password <-
-      readline(prompt = "Enter Password:"),
-    password <- getPass::getPass(msg = "Password", noblank = T)
-  )
-  
-  
-  #This combines your username and password with the IP address of the proxy
-  proxy_address<-paste0("http://",username,":",password,"@148.253.235.216:80")
-  
-  #This creates envirnmnet variables with the proxy details allowing R to connect.
-  Sys.setenv(http_proxy=proxy_address,
-             https_proxy=proxy_address)
-}
+proxy_connect <-
+  function(show_characters = FALSE,
+           url = "http://google.com") {
+    #Asks for username
+    ifelse(
+      show_characters,
+      username <-
+        readline(prompt = "Enter Username:"),
+      username <- getPass::getPass(msg = "Username", noblank = T)
+    )
+    
+    #Asks for password
+    ifelse(
+      show_characters,
+      password <-
+        readline(prompt = "Enter Password:"),
+      password <- getPass::getPass(msg = "Password", noblank = T)
+    )
+    
+    #If a URL is supplied, the proxy address is retrieved for it, otherwise it defaults to an external IP
+    proxy_ip <-
+      stringr::str_split(curl::ie_get_proxy_for_url(url), ";")[[1]][1]
+    
+    #This combines your username and password with the IP address of the proxy
+    proxy_address <-
+      paste0("http://", username, ":", password, "@", proxy_ip)
+    
+    #This creates envirnmnet variables with the proxy details allowing R to connect.
+    Sys.setenv(http_proxy = proxy_address,
+               https_proxy = proxy_address)
+  }
 
 
